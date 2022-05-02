55b72d750ab63981175c63bd72035f2dabef5dee
fgc <544942125@qq.com>
2022-05-02 10:47:46 +0800

    书籍分类列表页面完成

---
+++ pages/CList/CList.vue
@@ -0,0 +1,69 @@
+<template>
+	<view class="container">
+    <view class="header">
+      <text class="icon icon-fanhui"></text>
+      <text>科幻</text>
+      <text class="icon icon-caidan"></text>
+    </view>
+
+    <view class="list">
+      <view class="list-item">
+        <Book></Book>
+      </view>
+      <view class="list-item">
+        <Book></Book>
+      </view>
+      <view class="list-item">
+        <Book></Book>
+      </view>
+      <view class="list-item">
+        <Book></Book>
+      </view>
+      <view class="list-item">
+        <Book></Book>
+      </view>
+
+    </view>
+	</view>
+</template>
+
+<script>
+  import Book from "@/components/common/Book";
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		methods: {
+			
+		},
+    components:{
+      Book
+    }
+	}
+</script>
+
+<style lang="less" scoped>
+  .container{
+    padding: 0 24px;
+
+    //我的
+    .header {
+      display: flex;
+      height: 26px;
+      justify-content: space-between;
+      align-items: center;
+
+      text {
+        font-size: 22px;
+        font-weight: bold;
+        color: #FFFFFF;
+      }
+    }
+
+    .list-item{
+      margin: 30px 0;
+    }
+  }
+</style>
